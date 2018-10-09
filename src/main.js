6ddbe5da552a27c0d4b35b76adca1fbdafb68611
kebin320 <792754013@qq.com>
2018-10-09 21:36:43 +0800

    更新首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import index from './components/index.vue'
 
-Vue.config.productionTip = false
+// 引入element-ui / 需要在多个文件使用的库放在这里 
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+
+// 需要Vue把VueRouter use一下
+Vue.use(VueRouter);
+// 把elementUI也use一下
+Vue.use(ElementUI);
+// 规则
+const routes=[
+  {
+    path:'/',
+    component:index
+  },{
+    path:'/index',
+    component:index
+  }
+]
+const router = new VueRouter({
+  routes
+})
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // 将路由挂载到Vue实例上
+  router
 }).$mount('#app')
